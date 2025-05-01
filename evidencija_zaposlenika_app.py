08dd53315b38d3ab50f4c2832a28284ffcd4a010
1jukes <142570240+1jukes@users.noreply.github.com>
2025-05-01 23:28:44 +0200

    Update evidencija_zaposlenika_app.py

--- evidencija_zaposlenika_app.py
+++ evidencija_zaposlenika_app.py
@@ -548,7 +548,7 @@ def main():
             
             if e['next_physical_date'] and e['physical_required']:
                 try:
-            phys = datetime.strptime(e['next_physical_date'],'%Y-%m-%d').date()
+                    phys = datetime.strptime(e['next_physical_date'],'%Y-%m-%d').date()
                     if 0 <= (phys-date.today()).days <= 30:
                         phys_str = format_date(e['next_physical_date'])
                 except:
@@ -556,7 +556,7 @@ def main():
                     
             if e['next_psych_date'] and e['psych_required']:
                 try:
-            psych = datetime.strptime(e['next_psych_date'],'%Y-%m-%d').date()
+                    psych = datetime.strptime(e['next_psych_date'],'%Y-%m-%d').date()
                     if 0 <= (psych-date.today()).days <= 30:
                         psych_str = format_date(e['next_psych_date'])
                 except:
